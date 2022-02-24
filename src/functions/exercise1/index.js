254c76617dd88b89b1e66de3028f1be3b55593cd
ohmelopido@gmail.com <afdm@postal6.academiapostal.es.local>
2022-02-24 11:28:50 +0100

    EjercicioFunction1

--- src/functions/exercise1/index.js
+++ src/functions/exercise1/index.js
@@ -1,16 +1,51 @@
+// Functions 1
+// Define una función que busque y entregue (return) 
+// el menor valor de un array de números
+
+// Primero resuelvo como hayar el número menor, al recorrer un Array
+/*
+const numbers1 = [22,1,4,3,5,7,8,9,12,11];
+
+let min = numbers1[0];
+
+for (let i = 0; i < numbers1.length; i++) { // recorro el Array
+
+    if(numbers1[i] < min) { // si el valor que tiene la posición [i] del Array numbers1 
+                            // (la posición que toque según recorre el Array),
+                            // es menor que la variable min, posición [0] del Array numbers1                    
+        min = numbers1[i];  // se asigna (se guarda) ese valor en la variable min
+    }
+}
+
+console.log(min)
+*/
+
+// Defino una función y entreguo (return)
+
 const numbers1 = [22,1,4,3,5,7,8,9,12,11];
 const numbers2 = [22,7,4,3,5,7,8,9,12,11];
 const numbers3 = [22,6,4,4,5,7,8,9,12,11];
 const numbers4 = [22,9,6,8,5,7,8,9,12,11];
+ 
 
-function minorValue(array) {
+function minorValue(numbers1) { // function minorValue() vale para cualquier Array que se le pase, 
+                                // le puedo pasar numbers1 o llamarla miArray para realizar la function
+    let min = numbers1[0];
 
-    // Put your code here
-
+    for (let i = 0; i < numbers1.length; i++) {
+        if (numbers1[i] < min) {
+            min = numbers1[i];
+        }
+    }
+    return min; // retorno el valor guardado en la variable min
+    console.log(min)
 }
 
-const minor1 = minorValue(numbers1);
+const minor1 = minorValue(numbers1); // llamo a la function minorValue(numbers1)
+                                     // pasandole como parámetro el Array numbers1
+                                     // le asigno el resultado a la constante minor1
 console.log(minor1);
 console.log(minorValue(numbers2));
 console.log(minorValue(numbers3));
-console.log(minorValue(numbers4));
+console.log(minorValue(numbers4)); 
+
