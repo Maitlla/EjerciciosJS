d8cf779fa1e8973da466852819247f1ecadbf607
ohmelopido@gmail.com <afdm@postal6.academiapostal.es.local>
2022-02-25 13:44:48 +0100

    ejercicio10Arreglado

--- src/for and if/exercise10/index.js
+++ src/for and if/exercise10/index.js
@@ -4,19 +4,25 @@
 // intercambia la posición de ese elemento y el primero, y asi con todo el Array
 
 let numbers = [2, 1, 4, 5, 6, 3, 7, 8, 1, 4, 6, 8, 1, 5, 7, 8, 3]
-console.log("Desordenados", numbers)
-for(let outer = 0; outer < numbers.length; outer++){
-    for(let inner = outer + 1; inner < numbers.length; inner++
-    ){
-        if(numbers[outer] > numbers[inner]){
-            let temp = null;
-            temp = numbers[outer];
-            numbers[outer] = numbers[inner];
-            numbers[inner] = temp;
-            console.log("proceso", numbers)            
+console.log(numbers)
+for (let i = 0; i < numbers.length; i++) {
+    for (let j = i + 1; j < numbers.length; j++
+    ) {
+        if (numbers[i] > numbers[j]) { 
+            // null como hueco o valor vacío, se lo asigno a la variable casilla
+            let casilla = null; 
+            // asigno la posición de [i] en el Array numbers a la variable casilla
+            casilla = numbers[i];
+            // asigno la posición de [j] en el Array, a la posición de [i] en este caso
+            // es que paso el número que esta en segundo lugar al primer lugar
+            numbers[i] = numbers[j];
+            // la variable casilla que tenía asignada la posición de [i] en el Array,
+            // ahora se la asigno a [j] y así se hace el intercambio de posiciones
+            numbers[j] = casilla;
+            console.log(numbers)
         }
     }
 }
-console.log("Ordenados", numbers)
+console.log(numbers)
 
 
