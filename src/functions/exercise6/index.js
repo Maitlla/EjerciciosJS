fa0810f1a7fd1f6662ec8b97c484bc8c8d537edd
Maitlla <maitlla@gmail.com>
2022-03-03 00:25:37 +0100

    función6Final

--- src/functions/exercise6/index.js
+++ src/functions/exercise6/index.js
@@ -1,46 +1,46 @@
 // Functions 6
-// OPCIÓN A
-// Entre dos índices del Array numbers ordeno esa parte de manera ascendente
-
-
-
-// OPCIÓN B
-// Ordeno el Array numbers de manera ascendente, y entre dos índices del array me quedo con número menor
+// Entre dos índices del Array numbers ordeno sus elementos de manera ascendente, y me quedo con el valor menor
 
 const numbers = [11, 9, 13, 2, 5, 4, 9, 22, 12, 0, 16, 12, 35, 8, 74]
-// Función flecha
-//numbers.sort((a,b) => {return a-b});
-// Función anónima
-//numbers.sort(function(a,b) {return a-b});
+function burbuja(indiceInicio, indiceFinal, arr) {
+    let minor = numbers[indiceInicio];
+    indiceFinal = indiceFinal + 1;
+    console.log("Lista sin ordenar:");
+    console.log(numbers);
 
-// La función comparar me sirve para ordenar de manera ascendente cuando llame a la función .sort()
-function comparar(a, b) {
-    return a - b;
-}
-//console.log(numbers.sort(comparar));
-//console.log(numbers[0]);  
-
-function minorInRange(posicionInicial, posicionFinal, numbers) {
-    // asigno (guardo) el valor de posicionInicial del Array numbers a la variable minor
-    let minor = numbers[posicionInicial];
-    // recorro el Array entre dos índices determinados, (entre posicionInicial y posicionFinal) 
-    for (let i = posicionInicial; i <= posicionFinal; i++) {
-        // lo comparo con el valor guardado en la variable minor, si el número es más pequeño...
-        if (numbers[i] < minor) {
-            // ...lo asigno (guardo) en la variable minor
-            minor = numbers[i];
+    for (let i = 1; i < indiceFinal; i++) {
+        for (let j = indiceInicio; j < (indiceFinal - i); j++) {
+            if (arr[j] > arr[j + 1]) {
+                let temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+            if (numbers[i] < minor){ 
+                // ...lo asigno (guardo) en la variable minor
+                minor = numbers[i];
+            }
         }
     }
-    //console.log(minor);
-    return minor;
+     console.log("Lista ordenada:");
+     console.log(numbers);
+     console.log(minor);
+     return minor; 
 }
-// Llamo a la función .sort() pasandole como argumento la función comparar que he creado antes, para ordenar el Array numbers
-numbers.sort(comparar);
-// Para comprobar que el Array esta ordenado
-console.log(numbers);
-// Una vez ordenado el Array numbers, llamo a la función minorInRange
-// pasandole índice posicionInicial y índice posicionFinal, entre los que busco el número menor
-console.log(minorInRange(0, 3, numbers));
-console.log(minorInRange(4, 9, numbers));
-console.log(minorInRange(1, 5, numbers));
+burbuja(1, 11, numbers);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
--- src/functions/prueba.js
+++ src/functions/prueba.js
@@ -1,14 +1,42 @@
 // Ejercicio para probar
-// La función comparar me sirve para ordenar de manera ascendente cuando se llama a la función .sort()
-// El Array numbers llama a la función .sort() pasandole como argumento la función comparar previamente creada.
-// En la posición [0] del Array numbers esta el número menor
-/*
-const numbers = [11, 9, 13, 12] 
+
+const numbers = [11, 9, 13, 2, 5, 4, 9, 22, 12, 0, 16, 12, 35, 8, 74]
+// La función comparar me sirve para ordenar de manera ascendente cuando llame a la función .sort()
 function comparar(a, b) {
     return a - b;
 }
+//console.log(numbers.sort(comparar));
+//console.log(numbers[0]);  
+
+function minorInRange(posicionInicial, posicionFinal, numbers) {
+    // asigno (guardo) el valor de posicionInicial del Array numbers a la variable minor
+    let minor = numbers[posicionInicial];
+    // recorro el Array entre dos índices determinados, (entre posicionInicial y posicionFinal) 
+    for (let i = posicionInicial; i <= posicionFinal; i++) {
+        // lo comparo con el valor guardado en la variable minor, si el número es más pequeño...
+        if (numbers[i] < minor) {
+            // ...lo asigno (guardo) en la variable minor
+            minor = numbers[i];
+        }
+    }
+    //console.log(minor);
+    return minor;
+}
+// Llamo a la función .sort() pasandole como argumento la función comparar que he creado antes, para ordenar el Array numbers
 console.log(numbers.sort(comparar));
-console.log(numbers[0]);  */
+// Una vez ordenado el Array numbers, llamo a la función minorInRange
+// pasandole índice posicionInicial y índice posicionFinal, entre los que busco el número menor
+console.log(minorInRange(0, 3, numbers));
+console.log(minorInRange(4, 9, numbers));
+console.log(minorInRange(1, 5, numbers));
+
+
+
+
+
+
+
+
 
 
 /*
@@ -27,3 +55,19 @@ console.log('original:', arr.join()); // original: 1,5,9,40,80,200,700
 // otra manera de hacerlo directamente en el console.log 
 console.log('ordenado con función:', arr.sort(comparar));   */
 
+
+/*
+Función flecha
+numbers.sort((a,b) => {return a-b});
+Función anónima
+numbers.sort(function(a,b) {return a-b});
+
+function primera(param1){
+    
+}
+function segunda(param1){
+
+}
+primera(segunda); // Pasas la función
+primera(segunda());// Pasas el resultado de ejecutar la función
+*/
