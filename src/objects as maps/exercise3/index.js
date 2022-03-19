f2ba8ee8bf81f3e17a1075074ad9793b92cf8f09
Maitlla <maitlla@gmail.com>
2022-03-19 19:50:52 +0100

    Objetos/exercise3AMedias

--- src/objects as maps/exercise3/index.js
+++ src/objects as maps/exercise3/index.js
@@ -1,3 +1,6 @@
+// objects as maps 3
+// Adapta el código para poder ordenar los corredores del array `chrono` del ejercicio anterior
+
 const people = [
     {
         name: "Ana",
@@ -21,7 +24,51 @@ const people = [
     },
 ]
 
-// Your code here
+            //   NO los oredena
+function aFasterThanB(runnerA, runnnerB) {
+    if (runnerA.time < runnnerB.time) {
+        return true;
+    }else {
+        return false;
+    }
+    
+}
 
-sort(aFasterThanB, numbers)
-sort(bFasterThanA, numbers)
+function bFasterThanA(runnerA, runnerB) {
+    if (runnerA.time > runnerB.time) {
+        return true;
+    }else {
+        return false;
+    }
+}
+
+function swap (runnerA, runnerB, array) {
+    //let temporal = null;
+    temporal = array[runnerA.time];
+    array[runnerA.time] = array[runnerB.time];
+    array[runnerB.time] = temporal;
+}
+
+function sort (comparator, array) {
+    for ( let currentIdx = 0 ; currentIdx < array.length-1 ; currentIdx++) {
+        for ( let nextIdx = currentIdx+1 ; nextIdx < array.length ; nextIdx++ ) {
+            // si el resultado de comparar el indice actual (currentIdx) y el suguiente (nextIdx) es true
+            if (comparator(array[currentIdx], array[nextIdx])){ 
+                // intercambias las posiciones de currentIdx y nextIdx 
+                 swap (currentIdx, nextIdx, array); 
+            }
+        }
+    }
+}
+
+sort(aFasterThanB, people);
+console.log("Runner de menor a mayor ", JSON.stringify(people));
+sort(bFasterThanA, people);
+console.log("Runner de mayor a menor ", JSON.stringify(people));
+
+//sort(aFasterThanB, people);
+//console.log(people);
+//sort(bFasterThanA, people);
+//console.log(people);
+
+
