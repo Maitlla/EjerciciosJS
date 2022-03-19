0903b32c1af2fc053cd5f254f7391482697281c9
Maitlla <maitlla@gmail.com>
2022-03-19 18:38:14 +0100

    Functions/exercise8Resuelto

--- src/functions/exercise8/index.js
+++ src/functions/exercise8/index.js
@@ -1,23 +1,33 @@
-numbers = [4,2,9,6,1,0,3,6,5,2,9];
+// Functions 8
+// Cambia las posiciones del elemento en las posiciones `idxA` e `idxB` del array `array`  (si a > b)  (si b > a)
 
-function aGreatherThanB (a, b) {
-    // Put your code here
+const numbers = [4,2,9,6,1,0,3,6,5,2,9];
+
+function aGreatherThanB (a, b) { // ordena de manera ascendente
+    return a > b;
+    
 }
 
-function bGreatherThanA (a,b) {
-    // Put your code here
+function bGreatherThanA (a,b) { // ordena de maera descendente
+    return b > a;
+   
 }
 
 function swap (idxA, idxB, array) {
-    // Put your code here
+    x = array[idxA];
+    array[idxA] = array[idxB];
+    array[idxB] = x;
+  
 }
 
 function sort (comparator, array) {
     for ( let currentIdx = 0 ; currentIdx < array.length-1 ; currentIdx++) {
         for ( let nextIdx = currentIdx+1 ; nextIdx < array.length ; nextIdx++ ) {
-            if (
-                comparator(array[currentIdx], array[nextIdx])
-            ) swap (currentIdx, nextIdx, array)
+            // si el resultado de comparar el indice actual (currentIdx) y el suguiente (nextIdx) es true
+            if (comparator(array[currentIdx], array[nextIdx])){ 
+                // intercambias las posiciones de currentIdx y nextIdx 
+                 swap (currentIdx, nextIdx, array); 
+            }
         }
     }
 }
@@ -27,3 +37,5 @@ sort(aGreatherThanB, numbers);
 console.log(numbers);
 sort(bGreatherThanA, numbers);
 console.log(numbers);
+
+
